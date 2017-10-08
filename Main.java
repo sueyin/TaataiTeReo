c6fb8a2d53b3973fac50257338778e5c1bfa5bf2
sueyin <sueyin@users.noreply.github.com>
2017-10-08 19:01:14 +1300

    added view package

--- Main.java
+++ Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("../application/view/LoginPage.fxml"));
+        primaryStage.setTitle("Tatai Te Reo");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
