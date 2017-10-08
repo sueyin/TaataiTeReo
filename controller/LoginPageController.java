b0cdacfea3ead35c55223f0ec78ceb4bab22c5a4
sueyin <sueyin@users.noreply.github.com>
2017-10-08 19:12:49 +1300

    final set up

--- controller/LoginPageController.java
+++ controller/LoginPageController.java
@@ -1,9 +1,22 @@
 package application.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 
 public class LoginPageController {
+	
+	@FXML
+	public void handlePressAddUser(MouseEvent event) {
+		System.out.println("add user");
+	}
+	
+	@FXML
+	public void handlePressDeleteUser(MouseEvent event) {
+		System.out.println("delete user");
+	}
 
-
-
-
+	@FXML
+	public void handlePressEnter(MouseEvent event) {
+		System.out.println("enter");
+	}
 }
